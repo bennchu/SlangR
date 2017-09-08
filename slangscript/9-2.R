5ad7d5ca90d46b0c0021e4617781e5286582441a
bennchu <chu.benn@gmail.com>
2017-09-07 18:31:22 -0700

    Means

--- slangscript/9-2.R
+++ slangscript/9-2.R
@@ -6,8 +6,8 @@ unclean <- read_csv("U:/rawslangdat.csv")
 #for when I'm at home
 unclean<- read_csv("C:/Users/Branly Mclanbry/Downloads/rawslangdat.csv")
 cleanslang = tbl_df(unclean) %>%
-  clean_names() %>%
-  filter(progress == 100)%>%
+  clean_names()              %>%
+  filter(progress == 100)    %>%
   mutate(dv_conf    = (slangconf1 + slangconf2)/2,
          dv_simstud = (simstudent_1),
          dv_unslan  = (uncertslang_1),
@@ -24,12 +24,17 @@ cleanslang = tbl_df(unclean) %>%
          dv_ost     = (ostdv_ostdv_1 + ostdv_ostdv_2 + ostdv_ostdv_3 + ostdv_ostdv_4 + ostdv_ostdv_5 +
                        ostdv_ostdv_6 + ostdv_ostdv_7 + ostdv_ostdv_8 + ostdv_ostdv_9 +
                        ostdv_ostdv_10)/10,
-         iv_uncer   =  ifelse(is.na(hiunc_1),"low","high"),
-         iv_sl.cat=  ifelse(is.na(knownmanipcheck),"unknown","known"))
+         iv_uncer.cat   =  ifelse(is.na(hiunc_1),"low","high"),
+         iv_uncer.num   =  ifelse(is.na(hiunc_1),0,1),
+         iv_sl.cat      =  ifelse(is.na(knownmanipcheck),"unknown","known"),
+         iv_sl.num      =  ifelse(is.na(knownmanipcheck),0,1))
 
 #this is for cause i'm too lazy to keep looking cleanslang
-useable = cleanslang %>%
-  select(dv_conf,dv_simstud,dv_unslan,dv_ent,dv_proto,dv_hsuid,dv_unc,dv_ost,iv_uncer,iv_sl.cat)
-  
+thesis = cleanslang %>%
+  select(dv_conf,dv_simstud,dv_unslan,dv_ent,dv_proto,dv_hsuid,dv_unc,dv_ost,
+         iv_uncer.cat,iv_uncer.num,iv_sl.cat,iv_sl.num)
 
+tapply(thesis$dv_hsuid,thesis$iv_sl.cat, mean)
+tapply(thesis$dv_proto,thesis$iv_sl.cat, mean)
+tapply(thesis$dv_ost,thesis$iv_sl.cat, mean)
 
